8bdb01b301b12f5fb379076b3e7d8c2767dcb43f
Jory Braun <braunjory@gmail.com>
2018-02-03 14:29:21 -0500

    starting point

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,16 +1,34 @@
-<template>
-    <div>
-        {{ title }}
-    </div>
-</template>
-
 <script>
     export default {
         name: 'App',
         data() {
             return {
-                title: 'Hello World Now'
+                title: 'Tweet Analyzer 4000', 
+                tweets: [
+                    { 
+                        author: '@theRealTrump',
+                        message: 'im gonna build a wall',
+                        avatar: ''
+                    }
+                ]
             }
-        }
+        },
+        template: `
+            <div>
+                <header>
+                    {{ title }}
+                </header>
+                <main>
+                    <ul>
+                        <li v-for="(tweet, i) in tweets" :key="i">
+                            {{ tweet }}
+                        </li>
+                    </ul>
+                </main>
+                <footer>
+
+                </footer>
+            </div>
+        `
     }
 </script>
